bec8c74b1c812cb62794f111b7c3ad977b1f5fea
ZLODEYKOVICH <93370041+ZLODEYKOVICH@users.noreply.github.com>
2022-12-13 15:44:51 +0700

    ui class

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import sys
 from random import randint
-from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtGui import QPainter, QBrush, QColor
+from interface import Ui_Form
 
 
-class MainWindow(QWidget):
+class MainWindow(QWidget, Ui_Form):
     def __init__(self):
         super(MainWindow, self).__init__()
-        uic.loadUi('Ui.ui', self)
+        self.setupUi(self)
         self.btn_create_circle.clicked.connect(self.create_circle)
         self.draw = False
 
@@ -20,10 +20,11 @@ class MainWindow(QWidget):
         if self.draw:
             painter = QPainter()
             painter.begin(self)
-            painter.setBrush(QBrush(QColor('yellow')))
-            painter.setPen(QColor('yellow'))
+            color = randint(0, 255), randint(0, 255), randint(0, 255)
+            painter.setBrush(QBrush(QColor(*color)))
+            painter.setPen(QColor(*color))
             width = height = randint(50, 100)
-            painter.drawEllipse(randint(width, self.width() - width), randint(height, self.height() - height),
+            painter.drawEllipse(randint(0, self.width() - width), randint(height, self.height() - height),
                                 width, height)
             painter.end()
             self.draw = False
